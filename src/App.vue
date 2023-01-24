7baa8ecd0f099512ee8cbbd907bcec44e78b82aa
Nusalendra <nusalendraa@gmail.com>
2023-01-24 11:43:39 +0700

    computed getter & setter

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>Count : {{ nilai }}</div>
+  <button @click="add">ADD</button>
+
+  <div>Result : {{ result }}</div>
+</template>
+ 
+<script>
+import { reactive, ref, computed, toRefs } from "vue"
+
+export default {
+  setup() { 
+    const counter = reactive({
+      nilai: 0
+    });
+
+    const addNum = ref(1);
+
+    const add = () => {
+      addNum.value = 5;
+      counter.nilai++;
+    }
+
+    const result = computed({
+      get: () => counter.nilai + addNum.value,
+      set: (val) => addNum = val
+    })
+    return {
+      ...toRefs(counter),
+      add,
+      result
+    }
+  }
+}
+</script>
